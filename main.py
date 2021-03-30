fbee5bff8bbea02c487f3ac2d52c4be45112ab31
pandadrum <diya.tahilramani2010@gmail.com>
2021-03-30 00:23:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def myFunction(x, y):
+  #return x-y
+  #return x*y
+  #return x/y
+  #return x//y
+  print(x-y)
+  print(x*y)
+  print(x/y)
+  print(x//y)
+  return x+y
+#Calling functions
+#c=myFunction(6,9)
+#print(c)
+print(myFunction(6,9))
+print("-+-+-")
+print(myFunction(1,4))
+print("-+-+-")
+print(myFunction(8,3))
+print("-+-+-")
+print(myFunction(2,4))
+
+
+
